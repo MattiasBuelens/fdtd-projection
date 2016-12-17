2b9cf92e5b6c71c30c480e205f7237e0c4647c09
Mattias Buelens <mattias.buelens@gmail.com>
2016-12-17 18:26:00 +0100

    Memoizer: implement Function.

--- src/main/java/fdtd/util/Memoizer.java
+++ src/main/java/fdtd/util/Memoizer.java
@@ -4,19 +4,22 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 
-public class Memoizer<T, U> {
+public class Memoizer<T, U> implements Function<T, U> {
 
     private final Function<T, U> function;
-    private final Function<T, U> memoized;
     private final Map<T, U> cache = new ConcurrentHashMap<>();
 
     private Memoizer(final Function<T, U> function) {
         this.function = function;
-        this.memoized = (input) -> cache.computeIfAbsent(input, this.function);
+    }
+
+    @Override
+    public U apply(T t) {
+        return cache.computeIfAbsent(t, this.function);
     }
 
     public static <T, U> Function<T, U> memoize(final Function<T, U> function) {
-        return new Memoizer<>(function).memoized;
+        return new Memoizer<>(function);
     }
 
 }
