77e3c73b179c2825aa9725cb4c4a1f8e7b4cfd0e
Mattias Buelens <mattias.buelens@gmail.com>
2016-12-27 22:57:14 +0100

    TimedMessagePreset: make non-constructable.

--- src/main/java/fdtd/TimedMessagePreset.java
+++ src/main/java/fdtd/TimedMessagePreset.java
@@ -7,6 +7,9 @@ import java.util.List;
 
 public class TimedMessagePreset {
 
+    private TimedMessagePreset() {
+    }
+
     public static final List<TimedMessage> MESSAGES = Arrays.asList(
             new TimedMessageImpl("Now playing: DJ A", Duration.of(-1, ChronoUnit.HOURS)),
             new TimedMessageImpl("Now playing: DJ B", Duration.of(1, ChronoUnit.HOURS)),
